df7f5597ecaafba860523c666a3915387e6b28b5
mdenitti <massimodn77@gmail.com>
2023-04-13 10:55:19 +0200

    Relaties in models mentor & company

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Company;
 
 class Mentor extends Model
 {
     use HasFactory;
+
+    // belongs to many relationship with company
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
